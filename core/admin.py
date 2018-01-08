696a402d87ebb225273749226635e4297b069bb3
mathemartins <martins>
2018-01-08 14:43:04 +0100

    ok

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,2 @@
 from django.contrib import admin
 
-# Register your models here.
-
-from core.models import BannerContentControl
-
-admin.site.register(BannerContentControl)
--- core/models.py
+++ core/models.py
@@ -1,12 +1,2 @@
 from django.db import models
 
-# Create your models here.
-
-class BannerContentControl(models.Model):
-	text_header = models.CharField(max_length=99, default="Happy New Year", blank=True, null=True)
-	text_head_button = models.CharField(max_length=99, default="Coming Soon!!", blank=True, null=True)
-	content_statement = models.CharField(max_length=99, blank=True, null=True)
-
-	def __str__(self):
-		return self.text_header
-
