c17088c090daa89e36fa841ba405d12334314b12
mathemartins <martins>
2018-01-08 15:02:58 +0100

    ok

--- dev/settings/production.py
+++ dev/settings/production.py
@@ -173,6 +173,21 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default']['ENGINE'] = "django.contrib.gis.db.backends.postgis"
+
+
+CORS_REPLACE_HTTPS_REFERER       = True
+HOST_SCHEME                      = "https://"
+SECURE_PROXY_SSL_HEADER          = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT              = True
+SESSION_COOKIE_SECURE            = True
+CSRF_COOKIE_SECURE               = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS   = True
+SECURE_HSTS_SECONDS              = 1000000
+SECURE_FRAME_DENY   
+
 SESSION_COOKIE_SECURE = False
 CSRF_COOKIE_SECURE = False
 SECURE_SSL_REDIRECT = True
