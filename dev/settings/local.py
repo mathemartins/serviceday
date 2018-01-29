82ded2c8b48a0acc9b8fafdeb5d8542d6610c242
mathemartins <martins>
2018-01-29 01:21:44 +0100

    ok

--- dev/settings/local.py
+++ dev/settings/local.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 SECRET_KEY = '*gjsv)j9b2swuy@bmlf&a99pj5iretjat%ogrz9v(!@+e2#lm1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'hellotrackamechanic@gmail.com'
@@ -177,19 +177,19 @@ import dj_database_url
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 
-CORS_REPLACE_HTTPS_REFERER       = True
-HOST_SCHEME                      = "https://"
-SECURE_PROXY_SSL_HEADER          = ('HTTP_X_FORWARDED_PROTO', 'https')
-SECURE_SSL_REDIRECT              = True
-SESSION_COOKIE_SECURE            = True
-CSRF_COOKIE_SECURE               = True
-SECURE_HSTS_INCLUDE_SUBDOMAINS   = True
-SECURE_HSTS_SECONDS              = 1000000
-SECURE_FRAME_DENY                = True
+# CORS_REPLACE_HTTPS_REFERER       = True
+# HOST_SCHEME                      = "https://"
+# SECURE_PROXY_SSL_HEADER          = ('HTTP_X_FORWARDED_PROTO', 'https')
+# SECURE_SSL_REDIRECT              = True
+# SESSION_COOKIE_SECURE            = True
+# CSRF_COOKIE_SECURE               = True
+# SECURE_HSTS_INCLUDE_SUBDOMAINS   = True
+# SECURE_HSTS_SECONDS              = 1000000
+# SECURE_FRAME_DENY                = True
 
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-SECURE_SSL_REDIRECT = True
+# SESSION_COOKIE_SECURE = True
+# CSRF_COOKIE_SECURE = True
+# SECURE_SSL_REDIRECT = True
 
 
 # Password validation
